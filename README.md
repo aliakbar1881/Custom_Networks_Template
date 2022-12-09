e20afd3e014a73bbf26fe501e26cf958fa6e0d48
aliakbar1881 <aliakbar138118@gmail.com>
2022-12-09 01:17:01 +0000

    update readme and initial step

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Custom_Networks_Template
 This is a repo to base fuondation of another custom networks
+--------
+
+### how to run:
+- `setup env`:
+```bash
+conda env create -f environment.yaml
+```
+- activate env:
+```bash
+conda activate custom_networks_template
+```
+
+```
+cd Custom_Network_Template
+source init.sh
+```
+--------
+
+TODO:
+ - impelement logistic regression
+ - impelement neural network
---
+++ src/template_network/base.py
@@ -0,0 +1,109 @@
+"""
+base classes to override in any specific situation
+"""
+from abc import abstractmethod, ABC
+
+
+class Model(ABC):
+    """
+    network architecture
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        initialize instance properties
+        """
+
+    def __call__(self, *args, **kwargs):
+        """
+        initialize instance properties
+        """
+
+
+class Layer(ABC):
+    """
+    base class of any layer in network
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        initialize instance properties
+        """
+        self.weights = None
+
+    @abstractmethod
+    def __call__(self, *args, **kwargs):
+        """
+        run layer
+        """
+
+#      @abstractmethod
+    #  def _input(self, *args, **kwargs):
+    #      """
+    #      input fetaure
+    #      """
+    #
+    #  @abstractmethod
+    #  def _output(self, *args, **kwargs):
+    #      """
+    #      output
+    #      """
+
+
+class Node(ABC):
+    """
+    any node in each layer
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        initialize instance properties
+        """
+
+    @abstractmethod
+    def __call__(self, *args, **kwargs):
+        """
+        run node
+        """
+
+
+class Activation(ABC):
+    """
+    activation function for each Node
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        initialize instance properties
+        """
+
+    def __call__(self, *args, **kwargs):
+        """
+        run aactivation function
+        """
+
+
+class Optimizer(ABC):
+    """
+    optimizer for fastest convergence
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        initialize instance properties
+        """
+
+    def __call__(self, *args, **kwargs):
+        """
+        run Optimizer
+        """
+
+
+class Loss(ABC):
+    """
+    loss function to caluclate differences between perdiction and actual value
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        initiailize instance properties
+        """
+
+    def __call__(self, *args, **kwargs):
+        """
+        run Loss function
+        """
